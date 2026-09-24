fee3dad0e29a7b66b9ea551e7dcd72f101102742
Lee <264464762+leehart3000@users.noreply.github.com>
2026-09-24 14:21:36 +0100

    Add TypeDoc API documentation

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig(
-  { ignores: ["dist/", "**/.venv/"] },
+  { ignores: ["dist/", "**/.venv/", "api-docs/"] },
   js.configs.recommended,
   tseslint.configs.recommended,
 );
